600aec6002abe02b545e8969877a7ca63484904e
kiennt <kiennt0498@gmail.com>
2023-10-23 21:00:02 +0700

    qua

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://kiennguyen114.github.io/thu8-3/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Nụ cười em đánh rơi <br> Anh vô tình nhặt lấy <br>Kể từ ngày hôm ấy <br> Anh thành kẻ tương tư <br> Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'avata.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
